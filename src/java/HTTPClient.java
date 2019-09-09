6daf1f098e84523ca9756c14a177575c5f5d8b7e
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-09-09 14:25:45 -0400

    initial setup

---
+++ src/java/HTTPClient.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+public class HTTPClient {
+    private Socket socket;
+    private ServerSocket serverSocket;
+    private PrintWriter printWriter;
+    private BufferedReader bufferedReader;
+
+    //TODO: Start Client
+    public void start(){
+
+    }
+
+    //TODO: Close Client
+    public void end() {
+
+    }
+
+    //TODO: Serve Get Requests
+    public String get() {
+        return null;
+    }
+
+    //TODO: Serve Post Requests
+    public String post() {
+        return null;
+    }
+}

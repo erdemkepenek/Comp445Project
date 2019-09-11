d1373ccf6e428ac4a599cefffc64c76266c67136
Anthony Iatropoulos <anthonyiatropoulos@hotmail.com>
2019-09-11 14:06:12 -0400

    implemented GET requests

--- src/java/HTTPClient.java
+++ src/java/HTTPClient.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class HTTPClient {
     private Socket socket;
@@ -10,9 +11,8 @@ public class HTTPClient {
     //TODO: Start Client
     public void start(String host, int port) throws  IOException{
         socket = new Socket(host, port);
-        System.out.println("hello");
         printWriter = new PrintWriter(socket.getOutputStream(), true);
-        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
 
     }
 
@@ -21,30 +21,36 @@ public class HTTPClient {
         socket.close();
         printWriter.close();
         bufferedReader.close();
-
     }
 
     //TODO: Serve Get Requests
-    public String get() {
-        return null;
+    public void get() throws IOException{
+        printWriter.println("GET /status/418 HTTP/1.0");
+        printWriter.println("Host: httpbin.org");
+        printWriter.println("");
+        printWriter.flush();
+
+        bufferedReader.lines().forEach(System.out::println);
     }
 
     //TODO: Serve Post Requests
     public String post(String msg) {
         printWriter.println(msg);
-
+        return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         HTTPClient client = new HTTPClient();
         try {
             //trying to establish connection to the server
             client.start("httpbin.org",80);
-
+            client.get();
         } catch (UnknownHostException e) {
             System.err.println("The Connection has not been made");
         } catch (IOException e) {
             System.err.println("Connection is not established, because the server may have problems."+e.getMessage());
+        } finally {
+            client.end();
         }
 
     }
